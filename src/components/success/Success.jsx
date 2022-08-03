a6079e6ecc036f99a850525351999e62469ed555
Adarsh2k21 <adarsh.patel9619@gmail.com>
2022-08-04 03:59:58 +0530

    refactor

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { MdDone } from "react-icons/md";
+//import { MdDone } from "react-icons/md";
 import "./success.css";
-
-
 
 function Success() {
   return (
     <section className="main-container center ">
       <div className="flex flex-col gap-md pad-xl">
-       <div className='done-icon'><MdDone/></div>
+       <div className='done-icon'>
+      {/* <MdDone/> */} 
+       </div>
         <h3 className="main-heading">Congratulations, Eren!</h3>
         <p className="sub-heading">
           You have completed onboarding, you can start using Eden!
